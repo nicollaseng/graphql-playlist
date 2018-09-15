ba18c8b9a86d91f8c9b155ae7131de0fd8ee3205
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-09-15 15:50:51 -0300

    lodash installed and set resolve of rootquery

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require('graphql')
+const _ = require('lodash')
 
 const  { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+
+// dummy data
+var books = [ 
+    { name: 'Age of Empires', genre: 'Fantasy', id: '1'},
+    { name: 'Age of Persian', genre: 'Fantasy', id: '2'},
+    { name: 'Age of Times', genre: 'Fantasy', id: '3'},
+]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -18,8 +26,8 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             args: { id: { type: GraphQLString } },
             resolve(parent, args){
-                
                 // code to get data from db / other sources
+               return  _.find(books, { id: args.id })
             }
          }
     }
