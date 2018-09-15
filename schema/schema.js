d05428c58d3651fdab7149a952fe86b6cbc660d1
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-09-15 18:02:09 -0300

    books and authors as new types at root type

--- schema/schema.js
+++ schema/schema.js
@@ -74,6 +74,18 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent,  args){
                 return _.find(authors, { id: args.id } )
             }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args){
+                return authors
+            }
+        },
+        books:{
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return books
+            }
         }
     }
 })
