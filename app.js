70434062f7ea0a301887b084ef0b2e3fcf566085
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-09-15 19:00:22 -0300

    mongodb and mogoose set

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express()
+
+//connect to mlab database
+
+mongoose.connect('mongodb://admin:Petrobras2014@ds157762.mlab.com:57762/gql-nicolas')
+mongoose.connection.once('open', () => {
+    console.log('Connected to database')
+}).catch(error => console.log(error))
 
 app.use('/graphql', graphqlHTTP({
     schema,
